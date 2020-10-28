aac9c09fada80e959775f958bea4a5698319bd96
jieggii <jieggii.contact@gmail.com>
2020-10-28 20:43:24 +0300

    Add caster field to ImpossibleToCastError, some fixes

--- betterconf/exceptions.py
+++ betterconf/exceptions.py
@@ -5,13 +5,13 @@ class BetterconfError(Exception):
 class ImpossibleToCastError(BetterconfError):
     def __init__(self, val: str, caster: object):
         self.val = val
-        self.caster_name = caster.__class__.__name__
-        self.message = 'Could not cast "{}" with {}'.format(self.val, self.caster_name)
+        self.caster = caster
+        self.message = 'Could not cast "{}" with {}'.format(val, caster.__class__.__name__)
         super().__init__(self.message)
 
 
 class VariableNotFoundError(BetterconfError):
     def __init__(self, variable_name: str):
-        self.var_name = variable_name
+        self.variable_name = variable_name
         self.message = "Variable ({}) hasn't been found".format(variable_name)
         super().__init__(self.message)
