bc0c80c0091d05b1724011eaf64e6443c1dbebb6
RMuskovets <rmuskovets@gmail.com>
2020-10-31 09:15:04 +0200

    Add INIProvider
    Adds a provider that reads from INI files, with configurable:
    - delimiters (default ":" and "=")
    - comment prefixes (default "#" and ";")
    - files to read from

--- betterconf/providers.py
+++ betterconf/providers.py
@@ -1,3 +1,6 @@
+from configparser import ConfigParser
+from typing import Union, Any, Tuple, List
+
 class AbstractProvider:
     """Implement this class and pass to `field`"""
 
@@ -11,3 +14,17 @@ class EnvironmentProvider(AbstractProvider):
 
     def get(self, name: str) -> typing.Any:
         return os.getenv(name)
+
+class INIProvider(AbstractProvider):
+    """Provider that gets values from .INI files"""
+
+    def __init__(self, files: Union[str, List[str]],
+                 delimiters: Tuple[str] = ('=', ':'),
+                 comment_prefixes: Tuple[str] = ('#', ';')):
+        self.cfg = ConfigParser(delimiters=delimiters, comment_prefixes=comment_prefixes)
+        if isinstance(files, str):
+            files = [files]
+        self.cfg.read(files)
+
+    def get(self, name: str) -> str:
+        return self.cfg.get(name)
